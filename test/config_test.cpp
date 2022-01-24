87ea43b8cd6ba86f468a0cfb4726b6de31867b71
Jeff Tang <yayaya6d16@gmail.com>
2022-01-24 20:45:18 +0800

    implement config tests

---
+++ test/config_test.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include <cstdio>
+#include "config.hpp"
+#include "test_helper.hpp"
+
+class ConfigTest : public ::testing::Test {
+public:
+    ConfigTest() {
+        content = 
+            "strConfig=\"strConfig\"\n" 
+            "111\n"
+            "intConfig=100\n"
+            "=000\n";
+
+        logger = std::make_unique<Crystal::core::DefaultLogger>("");
+    }
+    ~ConfigTest() {}
+
+    void SetUp() override {
+        // create test file
+        std::ofstream testFile("config.txt");
+        testFile << content;
+        testFile.close();
+
+        Crystal::core::Config& c = Crystal::core::Config::GetConfig(logger.get());
+    }
+
+    void TearDown() override {
+        std::remove("config.txt");
+    }
+
+    std::string content;
+    std::unique_ptr<Crystal::core::Logger> logger;
+};
+
+TEST_F(ConfigTest, GetConfig_GetConfigSeveralTimes_DoNotLoadAgain) {
+    std::string output = generateTestOutputString([&]() {
+        Crystal::core::Config::GetConfig(logger.get());
+    });
+
+    EXPECT_EQ(output, "");
+}
+
+TEST_F(ConfigTest, GetStringConfigWithDefault_getStringConfig_getExpectedValue) {
+    std::string s1 = Crystal::core::Config::GetConfig().GetStringConfigWithDefault("", "123");
+    std::string s2 = Crystal::core::Config::GetConfig().GetStringConfigWithDefault("strConfig", "");
+
+    EXPECT_EQ(s1, "123");
+    EXPECT_EQ(s2, "strConfig");
+}
+
+TEST_F(ConfigTest, GetIntConfigWithDefault_getIntConfig_getExpectedValue) {
+    int i1 = Crystal::core::Config::GetConfig().GetIntConfigWithDefault("", 123);
+    int i2 = Crystal::core::Config::GetConfig().GetIntConfigWithDefault("intConfig", 0);
+
+    EXPECT_EQ(i1, 123);
+    EXPECT_EQ(i2, 100);
+}
